e7494cbdff17665ee84c806c05e899fed019a043
petrelevich <petrelevich@yandex.ru>
2025-09-24 11:32:29 +0300

    L23-redis

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,6 +39,11 @@ include("L21-jpql:homework-template")
 
 include("L22-cache")
 
+include("L23-redis:counter")
+include("L23-redis:data-source")
+include("L23-redis:data-transformer")
+include("L23-redis:data-listener")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
